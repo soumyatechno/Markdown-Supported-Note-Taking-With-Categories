62bb6c067b94b0ee6371cdbd0fa94ba4f52d90af
soumya <soumya.chakraborty@hoichoi.tv>
2023-11-22 18:39:37 +0530

    project setup is done

---
+++ Note Pad/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Markdown-Supported-Note-Taking-With-Categories
+
+A Note pad application build with latest features of react and typescript.
 Advance course using react and typescript
